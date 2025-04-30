55ec51767867105273eaa96ca7ee52af0685500a
sijiyaMT-08 <raphealleya8@gmail.com>
2025-05-01 00:41:33 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,253 +1,143 @@
-from django.conf import settings
-from django.contrib import admin, messages
-from django.contrib.admin.options import IS_POPUP_VAR
-from django.contrib.admin.utils import unquote
-from django.contrib.auth import update_session_auth_hash
-from django.contrib.auth.forms import (
-    AdminPasswordChangeForm,
-    AdminUserCreationForm,
-    UserChangeForm,
+from functools import partial
+
+from django.contrib.admin.checks import InlineModelAdminChecks
+from django.contrib.admin.options import InlineModelAdmin, flatten_fieldsets
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.forms import (
+    BaseGenericInlineFormSet,
+    generic_inlineformset_factory,
 )
-from django.contrib.auth.models import Group, User
-from django.core.exceptions import PermissionDenied
-from django.db import router, transaction
-from django.http import Http404, HttpResponseRedirect
-from django.template.response import TemplateResponse
-from django.urls import path, reverse
-from django.utils.decorators import method_decorator
-from django.utils.html import escape
-from django.utils.translation import gettext
-from django.utils.translation import gettext_lazy as _
-from django.views.decorators.csrf import csrf_protect
-from django.views.decorators.debug import sensitive_post_parameters
-
-csrf_protect_m = method_decorator(csrf_protect)
-sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
+from django.core import checks
+from django.core.exceptions import FieldDoesNotExist
+from django.forms import ALL_FIELDS
+from django.forms.models import modelform_defines_fields
 
 
-@admin.register(Group)
-class GroupAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
-    ordering = ("name",)
-    filter_horizontal = ("permissions",)
+class GenericInlineModelAdminChecks(InlineModelAdminChecks):
+    def _check_exclude_of_parent_model(self, obj, parent_model):
+        # There's no FK to exclude, so no exclusion checks are required.
+        return []
 
-    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
-        if db_field.name == "permissions":
-            qs = kwargs.get("queryset", db_field.remote_field.model.objects)
-            # Avoid a major performance hit resolving permission names which
-            # triggers a content_type load:
-            kwargs["queryset"] = qs.select_related("content_type")
-        return super().formfield_for_manytomany(db_field, request=request, **kwargs)
+    def _check_relation(self, obj, parent_model):
+        # There's no FK, but we do need to confirm that the ct_field and
+        # ct_fk_field are valid, and that they are part of a GenericForeignKey.
 
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    add_form_template = "admin/auth/user/add_form.html"
-    change_user_password_template = None
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                ),
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("username", "usable_password", "password1", "password2"),
-            },
-        ),
-    )
-    form = UserChangeForm
-    add_form = AdminUserCreationForm
-    change_password_form = AdminPasswordChangeForm
-    list_display = ("username", "email", "first_name", "last_name", "is_staff")
-    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
-    search_fields = ("username", "first_name", "last_name", "email")
-    ordering = ("username",)
-    filter_horizontal = (
-        "groups",
-        "user_permissions",
-    )
-
-    def get_fieldsets(self, request, obj=None):
-        if not obj:
-            return self.add_fieldsets
-        return super().get_fieldsets(request, obj)
-
-    def get_form(self, request, obj=None, **kwargs):
-        """
-        Use special form during user creation
-        """
-        defaults = {}
-        if obj is None:
-            defaults["form"] = self.add_form
-        defaults.update(kwargs)
-        return super().get_form(request, obj, **defaults)
-
-    def get_urls(self):
-        return [
-            path(
-                "<id>/password/",
-                self.admin_site.admin_view(self.user_change_password),
-                name="auth_user_password_change",
-            ),
-        ] + super().get_urls()
-
-    # RemovedInDjango60Warning: when the deprecation ends, replace with:
-    # def lookup_allowed(self, lookup, value, request):
-    def lookup_allowed(self, lookup, value, request=None):
-        # Don't allow lookups involving passwords.
-        return not lookup.startswith("password") and super().lookup_allowed(
-            lookup, value, request
-        )
-
-    @sensitive_post_parameters_m
-    @csrf_protect_m
-    def add_view(self, request, form_url="", extra_context=None):
-        with transaction.atomic(using=router.db_for_write(self.model)):
-            return self._add_view(request, form_url, extra_context)
-
-    def _add_view(self, request, form_url="", extra_context=None):
-        # It's an error for a user to have add permission but NOT change
-        # permission for users. If we allowed such users to add users, they
-        # could create superusers, which would mean they would essentially have
-        # the permission to change users. To avoid the problem entirely, we
-        # disallow users from adding users if they don't have change
-        # permission.
-        if not self.has_change_permission(request):
-            if self.has_add_permission(request) and settings.DEBUG:
-                # Raise Http404 in debug mode so that the user gets a helpful
-                # error message.
-                raise Http404(
-                    'Your user does not have the "Change user" permission. In '
-                    "order to add users, Django requires that your user "
-                    'account have both the "Add user" and "Change user" '
-                    "permissions set."
+        gfks = [
+            f
+            for f in obj.model._meta.private_fields
+            if isinstance(f, GenericForeignKey)
+        ]
+        if not gfks:
+            return [
+                checks.Error(
+                    "'%s' has no GenericForeignKey." % obj.model._meta.label,
+                    obj=obj.__class__,
+                    id="admin.E301",
                 )
-            raise PermissionDenied
-        if extra_context is None:
-            extra_context = {}
-        username_field = self.opts.get_field(self.model.USERNAME_FIELD)
-        defaults = {
-            "auto_populated_fields": (),
-            "username_help_text": username_field.help_text,
-        }
-        extra_context.update(defaults)
-        return super().add_view(request, form_url, extra_context)
-
-    @sensitive_post_parameters_m
-    def user_change_password(self, request, id, form_url=""):
-        user = self.get_object(request, unquote(id))
-        if not self.has_change_permission(request, user):
-            raise PermissionDenied
-        if user is None:
-            raise Http404(
-                _("%(name)s object with primary key %(key)r does not exist.")
-                % {
-                    "name": self.opts.verbose_name,
-                    "key": escape(id),
-                }
-            )
-        if request.method == "POST":
-            form = self.change_password_form(user, request.POST)
-            if form.is_valid():
-                # If disabling password-based authentication was requested
-                # (via the form field `usable_password`), the submit action
-                # must be "unset-password". This check is most relevant when
-                # the admin user has two submit buttons available (for example
-                # when Javascript is disabled).
-                valid_submission = (
-                    form.cleaned_data["set_usable_password"]
-                    or "unset-password" in request.POST
-                )
-                if not valid_submission:
-                    msg = gettext("Conflicting form data submitted. Please try again.")
-                    messages.error(request, msg)
-                    return HttpResponseRedirect(request.get_full_path())
-
-                user = form.save()
-                change_message = self.construct_change_message(request, form, None)
-                self.log_change(request, user, change_message)
-                if user.has_usable_password():
-                    msg = gettext("Password changed successfully.")
-                else:
-                    msg = gettext("Password-based authentication was disabled.")
-                messages.success(request, msg)
-                update_session_auth_hash(request, form.user)
-                return HttpResponseRedirect(
-                    reverse(
-                        "%s:%s_%s_change"
+            ]
+        else:
+            # Check that the ct_field and ct_fk_fields exist
+            try:
+                obj.model._meta.get_field(obj.ct_field)
+            except FieldDoesNotExist:
+                return [
+                    checks.Error(
+                        "'ct_field' references '%s', which is not a field on '%s'."
                         % (
-                            self.admin_site.name,
-                            user._meta.app_label,
-                            user._meta.model_name,
+                            obj.ct_field,
+                            obj.model._meta.label,
                         ),
-                        args=(user.pk,),
+                        obj=obj.__class__,
+                        id="admin.E302",
                     )
+                ]
+
+            try:
+                obj.model._meta.get_field(obj.ct_fk_field)
+            except FieldDoesNotExist:
+                return [
+                    checks.Error(
+                        "'ct_fk_field' references '%s', which is not a field on '%s'."
+                        % (
+                            obj.ct_fk_field,
+                            obj.model._meta.label,
+                        ),
+                        obj=obj.__class__,
+                        id="admin.E303",
+                    )
+                ]
+
+            # There's one or more GenericForeignKeys; make sure that one of them
+            # uses the right ct_field and ct_fk_field.
+            for gfk in gfks:
+                if gfk.ct_field == obj.ct_field and gfk.fk_field == obj.ct_fk_field:
+                    return []
+
+            return [
+                checks.Error(
+                    "'%s' has no GenericForeignKey using content type field '%s' and "
+                    "object ID field '%s'."
+                    % (
+                        obj.model._meta.label,
+                        obj.ct_field,
+                        obj.ct_fk_field,
+                    ),
+                    obj=obj.__class__,
+                    id="admin.E304",
                 )
-        else:
-            form = self.change_password_form(user)
+            ]
 
-        fieldsets = [(None, {"fields": list(form.base_fields)})]
-        admin_form = admin.helpers.AdminForm(form, fieldsets, {})
 
-        if user.has_usable_password():
-            title = _("Change password: %s")
+class GenericInlineModelAdmin(InlineModelAdmin):
+    ct_field = "content_type"
+    ct_fk_field = "object_id"
+    formset = BaseGenericInlineFormSet
+
+    checks_class = GenericInlineModelAdminChecks
+
+    def get_formset(self, request, obj=None, **kwargs):
+        if "fields" in kwargs:
+            fields = kwargs.pop("fields")
         else:
-            title = _("Set password: %s")
-        context = {
-            "title": title % escape(user.get_username()),
-            "adminForm": admin_form,
-            "form_url": form_url,
-            "form": form,
-            "is_popup": (IS_POPUP_VAR in request.POST or IS_POPUP_VAR in request.GET),
-            "is_popup_var": IS_POPUP_VAR,
-            "add": True,
-            "change": False,
-            "has_delete_permission": False,
-            "has_change_permission": True,
-            "has_absolute_url": False,
-            "opts": self.opts,
-            "original": user,
-            "save_as": False,
-            "show_save": True,
-            **self.admin_site.each_context(request),
+            fields = flatten_fieldsets(self.get_fieldsets(request, obj))
+        exclude = [*(self.exclude or []), *self.get_readonly_fields(request, obj)]
+        if (
+            self.exclude is None
+            and hasattr(self.form, "_meta")
+            and self.form._meta.exclude
+        ):
+            # Take the custom ModelForm's Meta.exclude into account only if the
+            # GenericInlineModelAdmin doesn't define its own.
+            exclude.extend(self.form._meta.exclude)
+        exclude = exclude or None
+        can_delete = self.can_delete and self.has_delete_permission(request, obj)
+        defaults = {
+            "ct_field": self.ct_field,
+            "fk_field": self.ct_fk_field,
+            "form": self.form,
+            "formfield_callback": partial(self.formfield_for_dbfield, request=request),
+            "formset": self.formset,
+            "extra": self.get_extra(request, obj),
+            "can_delete": can_delete,
+            "can_order": False,
+            "fields": fields,
+            "min_num": self.get_min_num(request, obj),
+            "max_num": self.get_max_num(request, obj),
+            "exclude": exclude,
+            **kwargs,
         }
 
-        request.current_app = self.admin_site.name
+        if defaults["fields"] is None and not modelform_defines_fields(
+            defaults["form"]
+        ):
+            defaults["fields"] = ALL_FIELDS
 
-        return TemplateResponse(
-            request,
-            self.change_user_password_template
-            or "admin/auth/user/change_password.html",
-            context,
-        )
+        return generic_inlineformset_factory(self.model, **defaults)
 
-    def response_add(self, request, obj, post_url_continue=None):
-        """
-        Determine the HttpResponse for the add_view stage. It mostly defers to
-        its superclass implementation but is customized because the User model
-        has a slightly different workflow.
-        """
-        # We should allow further modification of the user just added i.e. the
-        # 'Save' button should behave like the 'Save and continue editing'
-        # button except in two scenarios:
-        # * The user has pressed the 'Save and add another' button
-        # * We are adding a user in a popup
-        if "_addanother" not in request.POST and IS_POPUP_VAR not in request.POST:
-            request.POST = request.POST.copy()
-            request.POST["_continue"] = 1
-        return super().response_add(request, obj, post_url_continue)
+
+class GenericStackedInline(GenericInlineModelAdmin):
+    template = "admin/edit_inline/stacked.html"
+
+
+class GenericTabularInline(GenericInlineModelAdmin):
+    template = "admin/edit_inline/tabular.html"
